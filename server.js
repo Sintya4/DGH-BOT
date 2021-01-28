9803144e40d108725dce6794b5b23fba4c26f481
Glitch (hello-express) <none>
2021-01-28 05:50:36 +0000

    ⏲💇 Checkpoint ./server.js:27890737/3017

--- server.js
+++ server.js
@@ -1,15 +1,48 @@
-const Discord = require('discord.js'); 
-const client = new Discord.Client(); 
-client.once('ready', () => {
-  console.log('Ready!'); 
-  client.user.setActivity(`Test bot`);
+const Discord = require("discord.js");
+const client = new Discord.Client();
+//UPTIME ROBOT (WEB)
+client.on("ready", async () => {
+  console.log(`${client.user.tag} sudah online!`);
+  client.user.setActivity("Jangan Lupa Subscribe");
 });
+client.on("message", async message => {
+  if (message.content === "indonesia") message.reply(":flag_id:");
+//COMMAND BOT DI SERVER.JS
+const prefix = "!"
+if(!message.content.startsWith(prefix)) return null;
+let msg = message.content.toLowerCase();
+let args = message.content.slice(prefix.length).trim().split(" ");
+let cmd = args.shift().toLowerCase();
+let command = cmd;
 
-client.on("message", message => { 
-  if(message.content === "!ping") { 
-    return message.channel.send(`Pong ${client.ws.ping}`); //it will return message
+let commandFiles;
+try{
+  commandFiles = require(`./commands/${cmd}.js`)
+} catch (err) {
+  return message.reply("Command Not Found")
+}
+const db = require("quick.db")
+const now = Date.now()
+if(db.has(`cd_${message.author.id}`)) {
+  const expirationTime = db.get(`cd_${message.author.id}`) + 3000
+  if(now < expirationTime) {
+  const timeLeft = (expirationTime - now) / 1000;
+		return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${cmd}\` command.`);
   }
-});
+}
+  db.set(`cd_${message.author.id}`, now);
+  setTimeout(() => {
+    db.delete(`cd_${message.author.id}`)
+  },3000)
+try {
+  commandFiles.run(client, message, args)
+} catch (err) {
+  } finally {
+    console.log(`${message.author.tag} menggunakan command ${prefix}${cmd}`)
+  }
 
-client.login('NzgxNDYxNDY4NTI0MzE0NjI1.X79-tA.qgMtxd4BGEsLJTP1fX4n8ImQ1Lo'); //Paste Your Bot Token
+});
+client.login(process.env.TOKEN);
+
+//client.login(''); //Paste Your Bot Token
 
