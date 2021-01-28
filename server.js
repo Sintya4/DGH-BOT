fc45f3de5587a5e9c2f16d94f2287c58c2959767
Glitch (hello-express) <none>
2021-01-28 05:39:34 +0000

    👍🍢 Checkpoint ./server.js:27890737/2613 ./package.json:27890737/1288

--- server.js
+++ server.js
@@ -1,34 +1,15 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
-const app = express();
-
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+const Discord = require('discord.js'); 
+const client = new Discord.Client(); 
+client.once('ready', () => {
+  console.log('Ready!'); 
+  client.user.setActivity(`Test bot`);
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
+client.on("message", message => { 
+  if(message.content === "!ping") { 
+    return message.channel.send(`Pong ${client.ws.ping}`); //it will return message
+  }
 });
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+client.login('NzgxNDYxNDY4NTI0MzE0NjI1.X79-tA.qgMtxd4BGEsLJTP1fX4n8ImQ1Lo'); //Paste Your Bot Token
+
