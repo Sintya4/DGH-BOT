5f989446bd41b07e1d20d92920c598dc48a2066b
Glitch (mammoth-crystalline-vase) <none>
2021-01-30 23:00:26 +0000

    📯🌛 Checkpoint ./commands/Info/Corona.js:26780611/363

--- commands/Info/Corona.js
+++ commands/Info/Corona.js
@@ -1,24 +1,24 @@
 const { Message, MessageEmbed } = require("discord.js");
-//const { Default_Prefix, Color } = require("../../config.js");
 const Discord = require("discord.js");
-//const db = require("wio.db");
 const moment = require("moment");
 const fetch = require("node-fetch");
 const url = require("url");
 const api = require ("covidapi")
 module.exports = {
-  name: "covidall",
+  name: "covid",
   aliases: ["Covid-19"],
   category: "search",
   description: "Covid-19",
-  usage: "Covid-19",
+  usage: "covid <country>",
+  args: true,
   run: async (client, message, args) => {
     message.delete();
-   const data = await api.countries()
+    const a = args.join(" ")
+   const data = await api.countries({country: args.join(" ")})
     const coronaembed = new Discord.MessageEmbed()
     .setColor("#00f8ff")
-    .setTitle("Global Cases")
-    .setDescription("Number of cases may be differ from other sources")
+    .setTitle("Global Cases Covid-19")
+    .setDescription(`Number of Covid-19 cases on \`${a}\``)
     .addField("Cases", data.cases, true)
     .addField("Active", data.active, true)
     .addField("Cases Today", data.todayCases, true)
