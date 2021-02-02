43cd0ab235fe033d2eb88ae2fc389c2315f29a99
Glitch (mammoth-crystalline-vase) <none>
2021-02-02 00:16:37 +0000

    🚖🍼 Checkpoint ./commands/Moderation/setwelcome.js:26780611/1871 ./commands/Help/Settings.js:26780611/8098

--- commands/Help/Settings.js
+++ commands/Help/Settings.js
@@ -2,13 +2,16 @@ const { discord, Discord } = require("discord.js");
 const db = require("quick.db");
 const { MessageEmbed } = require("discord.js");
 //const Default_Prefix = require("../../config.json");
+
 module.exports = {
   name: "settings",
   description: "set message commands",
   usage: "set <key> <#channel>",
   category: "Help",
+  owner: true,
   run: async (client, message, args) => {
     //OWNER ONLY COMMAND
+    const channel = message.mentions.channels.first();
     message.delete();
     if (!message.author.id === `${message.guild.ownerID}`) {
       return message.channel
@@ -79,35 +82,40 @@ module.exports = {
           );
         }
         break;
-      case "level": {
-        //ARGUMENT
-        let channel = message.mentions.channels.first();
+      case "level":
+        {
+          //ARGUMENT
 
-        if (!channel) {
-          const wwww = new MessageEmbed()
+          if (!channel) {
+            const wwww = new MessageEmbed()
+              .setTitle("Settings Message")
+              .setDescription(
+                "<a:failed:798526823976796161> Please Mention the channel first"
+              )
+              .setColor("GREEN")
+              .setTimestamp();
+            return message.channel
+              .send(wwww)
+              .then(m => m.delete({ timeout: 12000 }).catch(e => {}));
+          }
+
+          //Now we gonna use quick.db
+          db.set(`level_${message.guild.id}`, channel.id);
+          const www = new MessageEmbed()
             .setTitle("Settings Message")
             .setDescription(
-              "<a:failed:798526823976796161> Please Mention the channel first"
+              `<a:success:798526789114134548> message has been set channel ${channel}`
             )
             .setColor("GREEN")
             .setTimestamp();
-          return message.channel
-            .send(wwww)
-            .then(m => m.delete({ timeout: 12000 }).catch(e => {}));
+          await message.channel
+            .send(www)
+            .then(m => m.delete({ timeout: 10000 }).catch(e => {}));
         }
-
-        //Now we gonna use quick.db
-        db.set(`level_${message.guild.id}`, channel.id);
-        const www = new MessageEmbed()
-          .setTitle("Settings Message")
-          .setDescription(
-            `<a:success:798526789114134548> message has been set channel ${channel}`
-          )
-          .setColor("GREEN")
-          .setTimestamp();
-        await message.channel
-          .send(www)
-          .then(m => m.delete({ timeout: 10000 }).catch(e => {}));
+        break;
+      case "welcome": {
+        
+       db.set(`welchannel_${message.guild.id}`, channel.id);
       };
     }
   }
