316592aa052e5e75c126ff9cdb1dd225e3556c90
Glitch (mammoth-crystalline-vase) <none>
2021-02-05 08:49:45 +0000

    📫🛩 Checkpoint ./commands/Misc/embed.js:26780611/3315

--- commands/Misc/embed.js
+++ commands/Misc/embed.js
@@ -5,31 +5,27 @@ module.exports = {
   name: "embed",
   category: "misc",
   description: "Get bot embed :/",
-  usage: "embed <msg>",
+  usage: "embed [...Text]\nembed -json [Raw JSON]",
   args: true,
   run: async (client, message, args) => {
     message.delete();
     const [key, ...value] = args;
     switch (key) {
-      case "inbot": {
+      case "-json": {
         try {
-          const json = JSON.parse(args.join(" "));
+          const json = JSON.parse(args.slice(1).join(" "));
           return message.channel.send({
             embed: json
           });
         } catch (error) {
           return message.channel.send(
-            `description: MessageEmbed Constructor! Supports JSON formatting if you know how to use them :P\`\`\`bash$ embed [...Message]$ embed -json {"title": "My title", "description": "My description"}$ embed -json {"author": {"name": "My author name", "icon_url": "url here"}, "description": "My description"}$ embed -json {"fields": [{"name": "My field name", "value": "My field value"}, {"name": "My field name", "value": "My field value", "inline": false}]}\`\`\``
+    `\`\`\`\n$ embed -json {"title": "My title","color":"Name color","description": "My description"}\n\`\`\`\`\`\`\n$ embed -json {"author": {"name": "My author name", "icon_url": "url here"}, "description": "My description"}\n\`\`\`\`\`\`\n$ embed -json {"fields": [{"name": "My field name", "value": "My field value"}, {"name": "My field name", "value": "My field value", "inline": false}]}\n\`\`\``
           );
         }
       }
     }
     return message.channel.send(
       new Discord.MessageEmbed()
-        .setAuthor(
-          message.author.tag,
-          message.author.displayAvatarURL({ dynamic: true }) ||
-            client.user.displayAvatarURL({ dynamic: true })
         )
         .setDescription(args.join(" "))
     );
