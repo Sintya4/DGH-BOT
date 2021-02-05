c1cced6a6910bc4cb530038d1083da81200c5933
Glitch (mammoth-crystalline-vase) <none>
2021-02-05 11:30:48 +0000

    🙆👙 Checkpoint ./commands/Misc/ping.js:26780611/582

--- commands/Misc/ping.js
+++ commands/Misc/ping.js
@@ -1,7 +1,6 @@
 const { Default_Prefix, Color, Support } = require("../../config.js");
 const Discord = require("discord.js");
 const db = require("wio.db");
-
 module.exports = {
   name: "ping",
   aliases: ["ms"],
@@ -9,6 +8,18 @@ module.exports = {
   description: "Show Bot Ping!",
   usage: "Ping",
   run: async (client, message, args) => {
-     return message.channel.send(`Pong - ${Math.round(client.ws.ping)}`)
+    const ms = require("ms")
+    let time = '3s'
+    setTimeout (function(){
+      message.edit("Loading")
+      },ms(time))
+    let time1 = '3s'
+    setTimeout (function(){
+      message.edit("Loading")
+      },ms(time1))
+    let time2 = '3s'
+    setTimeout (function(){
+       return message.channel.send(`Pong - ${Math.round(client.ws.ping)}`)
+    },ms(time2))
   }
 };
