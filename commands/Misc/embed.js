601e4a90a694b35d8918ad9c6d2feb324e4767fd
Glitch (mammoth-crystalline-vase) <none>
2021-02-05 08:26:47 +0000

    ✈️🏦 Checkpoint ./commands/Misc/embed.js:26780611/4051

--- commands/Misc/embed.js
+++ commands/Misc/embed.js
@@ -1,6 +1,6 @@
 const db = require("quick.db");
 
-const DISCORD = require("discord.js");
+const Discord = require("discord.js");
 module.exports = {
   name: "embed",
   category: "misc",
@@ -9,14 +9,18 @@ module.exports = {
   args: true,
   run: async (client, message, args) => {
     message.delete();
-    let embedtext = args.slice(0).join(" ");
-    if (!embedtext)
-      return message.channel
-        .send("Enter the words first!")
-        .then(m => m.delete({ timeout: 5000 }).catch(e => {}));
-    let embed = new DISCORD.MessageEmbed()
-      .setDescription(embedtext)
-      .setColor("GREEN");
-    message.channel.send(embed);
-  }
-};
+    if (message.flags.includes('json')) {
+            try {
+                const json = JSON.parse(args.join(' '));
+                return message.channel.send({
+                    embed: json
+                })
+            } catch (error) {
+                return message.channel.send(`\`ERROR\`\n\`\`\`xl\n${client.shorten(error, 512)}\n\`\`\``);
+            }
+        }
+        return message.channel.send(new Discord.MessageEmbed()
+            .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }) || client.user.displayAvatarURL({ dynamic: true }))
+            .setDescription(client.shorten(args.join(' '), 2048))
+        );
+    }}
