4dd4a2d2a4fba817f47720531901c3b745787ab8
Glitch (mammoth-crystalline-vase) <none>
2021-02-05 08:38:15 +0000

    🛬🎤 Checkpoint ./commands/Misc/embed.js:26780611/3490

--- commands/Misc/embed.js
+++ commands/Misc/embed.js
@@ -9,18 +9,29 @@ module.exports = {
   args: true,
   run: async (client, message, args) => {
     message.delete();
-    if (message.flags.includes('json')) {
-            try {
-                const json = JSON.parse(args.join(' '));
-                return message.channel.send({
-                    embed: json
-                })
-            } catch (error) {
-                return message.channel.send(`\`ERROR\`\n\`\`\`xl\n${client.shorten(error, 512)}\n\`\`\``);
-            }
+    const [key, ...value] = args;
+    switch (key) {
+      case "inbot": {
+        try {
+          const json = JSON.parse(args.join(" "));
+          return message.channel.send({
+            embed: json
+          });
+        } catch (error) {
+          return message.channel.send(
+            `description: MessageEmbed Constructor! Supports JSON formatting if you know how to use them :P\`\`\`bash$ embed [...Message]$ embed -json {"title": "My title", "description": "My description"}$ embed -json {"author": {"name": "My author name", "icon_url": "url here"}, "description": "My description"}$ embed -json {"fields": [{"name": "My field name", "value": "My field value"}, {"name": "My field name", "value": "My field value", "inline": false}]}\`\`\``
+          );
         }
-        return message.channel.send(new Discord.MessageEmbed()
-            .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }) || client.user.displayAvatarURL({ dynamic: true }))
-            .setDescription(client.shorten(args.join(' '), 2048))
-        );
-    }}
+      }
+    }
+    return message.channel.send(
+      new Discord.MessageEmbed()
+        .setAuthor(
+          message.author.tag,
+          message.author.displayAvatarURL({ dynamic: true }) ||
+            client.user.displayAvatarURL({ dynamic: true })
+        )
+        .setDescription(args.join(" "))
+    );
+  }
+};
