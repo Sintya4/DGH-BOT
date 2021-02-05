ebe59dbd13f56dccd9327cc6da7a07d25dddf823
Glitch (mammoth-crystalline-vase) <none>
2021-02-05 11:07:37 +0000

    💕👽 Checkpoint ./commands/Misc/sayc.js:26780611/453

--- commands/Misc/sayc.js
+++ commands/Misc/sayc.js
@@ -14,14 +14,20 @@ module.exports = {
     let chnnel = message.guild.channels.cache.find(
       x => x.id === db.get(`say_${message.guild.id}`, channel.id)
     );
+  await db.set(`say_${message.guild.id}`, channel.id);
 
     const [key, ...value] = args;
     switch (key) {
       case "-json": {
-        await db.set(`say_${message.guild.id}`, channel.id);
         const COLOR = args[2]
+        if(!COLOR) {
+             return message.channel
+        .send("<a:failed:798526823976796161> Please Give Color Embed")
+        .then(m => m.delete({ timeout: 5000 }).catch(e => {}));
+    }
+   
           const embed = new Discord.MessageEmbed()
-            .setColor(`)
+            .setColor(`${COLOR || "GREEN"}`)
             .setDescription(args.slice(3).join(" ")
         );
 
@@ -41,8 +47,7 @@ module.exports = {
         .send(`${message.author}, sayc <channel> <msg>`)
         .then(m => m.delete({ timeout: 5000 }).catch(e => {}));
     let say = args.slice(1).join(" ");
-    await db.set(`say_${message.guild.id}`, channel.id);
-
+  
     //  const Channel = member.guild.channels.cache.get("797491226567114753"); //insert channel id that you want to send to
     chnnel.send(say);
   }
