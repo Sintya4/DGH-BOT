4cfdb56ab7450ac4c487551686d8881f82824ea5
Glitch (mammoth-crystalline-vase) <none>
2021-02-05 11:45:30 +0000

    😺👻 Checkpoint ./commands/Misc/ping.js:26780611/1047

--- commands/Misc/ping.js
+++ commands/Misc/ping.js
@@ -8,18 +8,47 @@ module.exports = {
   description: "Show Bot Ping!",
   usage: "Ping",
   run: async (client, message, args) => {
-    const ms = require("ms")
-    let time = '3s'
+    /*const ms = require("ms")
+     const msg = await message.channel.send(`Loading`)
+    let time1 = '2s'
     setTimeout (function(){
-      message.edit("Loading")
-      },ms(time))
-    let time1 = '3s'
-    setTimeout (function(){
-      message.edit("Loading")
+      msg.edit("Loading.")
       },ms(time1))
     let time2 = '3s'
     setTimeout (function(){
-       return message.channel.send(`Pong - ${Math.round(client.ws.ping)}`)
-    },ms(time2))
+      msg.edit("Loading..")
+      },ms(time2))
+    let time3 = '4s'
+    setTimeout (function(){
+      msg.edit("Loading...")
+      },ms(time3))
+    let time4 = '5s'
+    setTimeout (function(){
+      msg.edit("Loading....")
+      },ms(time4))
+    let time5 = '6s'
+    setTimeout (function(){
+      msg.edit("Loading.....")
+      },ms(time5))
+    let time6 = '7s'
+    setTimeout (function(){
+      msg.edit("Loading")
+      },ms(time6))
+    let time7 = '8s'
+    setTimeout (function(){
+      msg.edit("Loading.")
+      },ms(time7))
+    let time8 = '9s'
+    setTimeout (function(){
+      msg.edit("Loading..")
+      },ms(time8))
+    let time9 = '10s'
+    setTimeout (function(){
+      msg.edit("Loading...")
+      },ms(time9))*/
+    let time10 = '11s'
+    setTimeout (function(){
+       msg.edit(`Pong - ${Math.round(client.ws.ping)}`)
+    },ms(time10)
   }
 };
