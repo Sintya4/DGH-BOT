cf2fbfbc490c5cd1bbc69c75fb829163947f30bf
Glitch (mammoth-crystalline-vase) <none>
2021-02-11 15:21:53 +0000

    👽💒 Checkpoint ./commands/Misc/snipe.js:26780611/2584

--- commands/Misc/snipe.js
+++ commands/Misc/snipe.js
@@ -33,26 +33,23 @@ Clink :x: to clear this message`
     message.channel.send(embed).then(m => {
       m.react("✅");
       m.react("❌");
-      client.on("messageReactionAdd", async (reaction, user) => {
-        if (user.bot) return; // If the user was a bot, return.
-const filter = (reaction, user) => {
+    const filter = (reaction, user) => {
             return (
                 ["❌", "✅"].includes(reaction.emoji.name) &&
                 user.id === message.author.id
             );
         };
-        embed.awaitReactions(filter, { max: 1, time: 300000, errors: ["time"] }).then(collected => {
+
+        m.awaitReactions(filter, { max: 1, time: 300000, errors: ["time"] }).then(collected => {
         const reaction = collected.array()[collected.size - 1]
-})
-        if (!reaction.message.guild) return; // If the user was reacting something but not in the guild/server, ignore them.
+
+            if (!reaction.message.guild) return; // If the user was reacting something but not in the guild/server, ignore them.
 
         if (reaction.emoji.name === "❌") {
           message.channel.bulkDelete(1);
         }
-      });
+      })})}};
       // message.channel.send("If there is a new delete message, you can use this command").then(m=>m.delete({timeout:12000}).catch(e=>{}))
 
       // }})}})
-    });
-  }
-};
+    
