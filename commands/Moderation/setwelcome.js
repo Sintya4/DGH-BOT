0d9e9ad2264f2a4523e157be09ff9b5326f26588
Glitch (mammoth-crystalline-vase) <none>
2021-02-12 04:18:26 +0000

    😋🌵 Checkpoint ./commands/Moderation/setwelcome.js:26780611/1005

--- commands/Moderation/setwelcome.js
+++ commands/Moderation/setwelcome.js
@@ -4,25 +4,35 @@ const db = require("quick.db")
 module.exports = {
   name: "welcomer",
   category: "moderation",
+  args: true,
+  usage: "welcomer channel <ChannelMention>\nwelcomer message <...Message>`^(Must include ({member},{username},{tag},{server},{size}) for this to work!)^`\nwelcomer testmessage",
   description: "Set the welcome",
   run: (client, message, args) => {
     const channel = message.mentions.channels.first()
+    const join = args[1]
+    const jo = args[1]
     const [key, ...value] = args;
     switch (key) {
-    case "welcomechannel":
+    case "channel":
         {
     if(!channel) {
     return message.channel.send(`${client.emojis.error} Invalid channel... Try again...`)
+    };
+    if(!join) {
+    return message.channel.send(`${client.emojis.error} Invalid channel... Try again...`)
+    };
     db.set(`welchannel_${message.guild.id}`, channel.id)
     const welcome = new Discord.MessageEmbed()
     .setDescription (`**Done** From now on I will send welcome message in ${channel} when someone joins the server`)
     message.channel.send(welcome)
    };
-        };
     break;
-    case "welcomemessage":
+    case "message":
         {
-    const msg = args.slice(1).join(" ")
+     if(!jo) {
+    return message.channel.send(`${client.emojis.error} Please give a message ({member},{username},{tag},{server},{size})`)
+    };
+   const msg = args.slice(1).join(" ")
     db.set(`message_${message.guild.id}`, msg)
     const message = new Discord.MessageEmbed()
     .setDescription (`**Done** From now on I will send\n\`${msg}\``)
