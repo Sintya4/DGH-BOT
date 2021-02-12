5ba8c2bdd4fb6337c55052d0559093360254e79a
Glitch (mammoth-crystalline-vase) <none>
2021-02-12 03:09:48 +0000

    💠🎊 Checkpoint ./commands/Moderation/setwelcome.js:26780611/910

--- commands/Moderation/setwelcome.js
+++ commands/Moderation/setwelcome.js
@@ -2,23 +2,27 @@ const Discord = require("discord.js")
 const db = require("quick.db")
 
 module.exports = {
-  name: "setwelcome",
+  name: "welcomer",
   category: "moderation",
-  usage: "setwelcome <#channel>",
+  usage: "welcomer channel <ChannelMention>\nwelcomer message <...Message>`^({member},{username},{tag},{server},{size})^`welcomer [enable|disable]",
   description: "Set the welcome channel",
   run: (client, message, args) => {
+    const channel = message.mentions.channels.first()
+    const [key, ...value] = args;
+    switch (key) {
+    case "channel":
+        {
+          //ARGUMENT
     
-    let channel = message.mentions.channels.first()
-    const msh =args.slice(1).join(" ")// JSON.parse(args.slice(1).join(" "));
        
     if(!channel) {
-      return message.channel.send("Please Mention the channel first")
+      return message.channel.send(${client.emojis.error})
     }
     
     //Now we gonna use quick.db
     
     db.set(`welchannel_${message.guild.id}`, channel.id)
-    db.set(`messag_${message.guild.id}`, msh)
+    db.set(`message_${message.guild.id}`, msh)
     
     message.channel.send(`Welcome Channel is seted as ${channel}`)
   }
