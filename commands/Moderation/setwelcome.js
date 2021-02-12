e030edb9bb086da071cfc92c1a5078cbedb26bf4
Glitch (mammoth-crystalline-vase) <none>
2021-02-12 03:43:47 +0000

    🎑🧀 Checkpoint ./commands/Moderation/setwelcome.js:26780611/1724

--- commands/Moderation/setwelcome.js
+++ commands/Moderation/setwelcome.js
@@ -15,17 +15,25 @@ module.exports = {
     if(!channel) {
       return message.channel.send(`${client.emojis.error} Invalid channel... Try again...`)
      db.set(`welchannel_${message.guild.id}`, channel.id)
-     message.channel.send(`Welcome channel has been set to ${channel}.`)
+       const welcome = new Discord.MessageEmbed()
+    .setDescription (`**Done** From now on I will send welcome`)
+  message.channel.send(`Welcome channel has been set to ${channel}.`)
    };
         };
-    
+    break;
+        case "message":
+        {
+    const msg = args.slice(1).join(" ")
+    db.set(`message_${message.guild.id}`, msg)
+    const setup = new Discord.MessageEmbed()
+    .setDescription (`**Done** From now on I will send\n\`${msg}\``)
+    message.channel.send(setup)
+   }}
+}}
+  
     /*  let chx = db.set(`welchannel_${message.guild.id}`, channel.id)
    const sender = client.channels.cache.get(chx);
     sender.send(`Welcome to server ${message.author}`)
   
   Now we gonna use quick.db*/
     
-    db.set(`message_${message.guild.id}`, msh)
-    
-  }
-}}
