ae0190a3994ff8c992518d47321a2694a3494aea
Glitch (mammoth-crystalline-vase) <none>
2021-02-12 03:55:45 +0000

    ❣️🌇 Checkpoint ./commands/Moderation/setwelcome.js:26780611/1089

--- commands/Moderation/setwelcome.js
+++ commands/Moderation/setwelcome.js
@@ -4,32 +4,42 @@ const db = require("quick.db")
 module.exports = {
   name: "welcomer",
   category: "moderation",
-  usage: "welcomer channel <ChannelMention>\nwelcomer message <...Message>`^({member},{username},{tag},{server},{size})^`welcomer [enable|disable]",
-  description: "Set the welcome channel",
+  description: "Set the welcome",
   run: (client, message, args) => {
     const channel = message.mentions.channels.first()
     const [key, ...value] = args;
     switch (key) {
-    case "channel":
+    case "welcomechannel":
         {
     if(!channel) {
-      return message.channel.send(`${client.emojis.error} Invalid channel... Try again...`)
-     db.set(`welchannel_${message.guild.id}`, channel.id)
-       const welcome = new Discord.MessageEmbed()
-    .setDescription (`**Done** From now on I will send welcome`)
-  message.channel.send(`Welcome channel has been set to ${channel}.`)
+    return message.channel.send(`${client.emojis.error} Invalid channel... Try again...`)
+    db.set(`welchannel_${message.guild.id}`, channel.id)
+    const welcome = new Discord.MessageEmbed()
+    .setDescription (`**Done** From now on I will send welcome message in ${channel} when someone joins the server`)
+    message.channel.send(welcome)
    };
         };
     break;
-        case "message":
+    case "message":
         {
     const msg = args.slice(1).join(" ")
     db.set(`message_${message.guild.id}`, msg)
-    const setup = new Discord.MessageEmbed()
+    const message = new Discord.MessageEmbed()
     .setDescription (`**Done** From now on I will send\n\`${msg}\``)
-    message.channel.send(setup)
-   }}
-}}
+    message.channel.send(message)
+  };
+    break;
+    case "testmessage":
+        {
+    const test = new Discord.MessageEmbed()
+    .setTitle (`**Testing Member Join**`)
+    . setDescription ("I'll
+    
+    
+    
+    message.channel.send(test)
+   }};
+}};
   
     /*  let chx = db.set(`welchannel_${message.guild.id}`, channel.id)
    const sender = client.channels.cache.get(chx);
