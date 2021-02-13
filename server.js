c99f0a371cbdd634df702707a53dc241705054a2
Glitch (mammoth-crystalline-vase) <none>
2021-02-13 06:07:10 +0000

    🐂🐣 Checkpoint ./server.js:26780611/3449 ./welcome.js:26780611/3979 ./.glitch-assets:26780611/321 ./welcome.txt:26780611/126835

--- server.js
+++ server.js
@@ -24,6 +24,8 @@ client.queue = new Map();
 client.config = require("./config/bot");
 client.message = require("./config/bot");
 client.emotes = client.config.emojis;
+const welcome = require("./welcome");
+welcome(client);
 /*=====================================================================*/
 //<ACTIVITY>
 client.on("ready", async () => {
@@ -252,84 +254,9 @@ MANAGE_EMOJIS'*/
   /*====================================================================*/
   //<COMMAND EP/LEVEL>
   return addexp(message);
-});
+  });
 
-/*====================================================================*/
-client.on("guildMemberAdd", async member => {
- /*const canva = require("canvas-constructor");
-  const path = require("path");
-  const snekfetch = require("snekfetch");
-  const fs = require("fs");
-  const superagent = require("superagent");
-                       
-const image = new Canvas.Welcome()
-  .setUsername("xixi52")
-  .setDiscriminator("0001")
-  .setMemberCount("140")
-  .setGuildName("Server DEV")
-  .setAvatar("https://cdn.discordapp.com/attachments/807204846850539520/809408353771454544/SPOILER_20201228_090307.jpg")
-  .setColor("border", "#8015EA")
-  .setColor("username-box", "#8015EA")
-  .setColor("discriminator-box", "#8015EA")
-  .setColor("message-box", "#8015EA")
-  .setColor("title", "#8015EA")
-  .setColor("avatar", "#8015EA")
-  .setBackground("https://cdn.discordapp.com/attachments/807204846850539520/809408353771454544/SPOILER_20201228_090307.jpg")
-  .toAttachment();
-  */
-  const Canvas = require("canvas");
- if(!member.guild) return;
-      //create a new Canvas
-      const canvas = Canvas.createCanvas(1772, 633);
-      //make it "2D"
-      const ctx = canvas.getContext('2d');
-      //set the Background to the welcome.png
-      const background = await Canvas.loadImage(`https://cdn.glitch.com/02e867ae-7c7c-4637-ace7-66ea251fe9d5%2Fthumbnails%2Fwelcome.png?1613195262594`);
-      ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-      ctx.strokeStyle = '#f2f2f2';
-      ctx.strokeRect(0, 0, canvas.width, canvas.height);
-      //set the first text string 
-      var textString3 = `${member.user.username}`;
-      //if the text is too big then smaller the text
-      if (textString3.length >= 14) {
-        ctx.font = 'bold 100px Genta';
-        ctx.fillStyle = '#f2f2f2';
-        ctx.fillText(textString3, 720, canvas.height / 2 + 20);
-      }
-      //else dont do it
-      else {
-        ctx.font = 'bold 150px Genta';
-        ctx.fillStyle = '#f2f2f2';
-        ctx.fillText(textString3, 720, canvas.height / 2 + 20);
-      }
-      //define the Discriminator Tag
-      var textString2 = `#${member.user.discriminator}`;
-      ctx.font = 'bold 40px Genta';
-      ctx.fillStyle = '#f2f2f2';
-      ctx.fillText(textString2, 730, canvas.height / 2 + 58);
-      //define the Member count
-      var textString4 = `Member #${member.guild.memberCount}`;
-      ctx.font = 'bold 60px Genta';
-      ctx.fillStyle = '#f2f2f2';
-      ctx.fillText(textString4, 750, canvas.height / 2 + 125);
-      //get the Guild Name
-      var textString4 = `${member.guild.name}`;
-      ctx.font = 'bold 60px Genta';
-      ctx.fillStyle = '#f2f2f2';
-      ctx.fillText(textString4, 700, canvas.height / 2 - 150);
-      //create a circular "mask"
-      ctx.beginPath();
-      ctx.arc(315, canvas.height / 2, 250, 0, Math.PI * 2, true);//position of img
-      ctx.closePath();
-      ctx.clip();
-      //define the user avatar
-      const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
-      //draw the avatar
-      ctx.drawImage(avatar, 65, canvas.height / 2 - 250, 500, 500);
-      //get it as a discord attachment
-      const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
-      //define the welcome embed
-  let chx = db.get(`welchannel_${member.guild.id}`);
+/*  let chx = db.get(`welchannel_${member.guild.id}`);
   let ch = db
     .get(`message_${member.guild.id}`)
     .replace(`{user}`,member)// Member mention substitution
@@ -349,7 +276,7 @@ const image = new Canvas.Welcome()
   sender.send({
     embed: json
   });
-});
+});*/
 /*====================================================================*/
 
 /*====================================================================*/
