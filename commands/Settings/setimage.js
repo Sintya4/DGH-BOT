d32b2923f12fb71a9a4b994b7aa8c681069b0f94
Glitch (mammoth-crystalline-vase) <none>
2021-02-15 05:37:43 +0000

    🏖💴 Checkpoint ./commands/Settings/setimage.js:26780611/1936

---
+++ commands/Settings/setimage.js
@@ -0,0 +1,57 @@
+const Discord = require("discord.js");
+const db = require("quick.db");
+
+module.exports = {
+  name: "setimage",
+  category: "setting",
+  args: true,
+  usage: "setimage <key //welcome/leave> <channel>",
+  description: "Set the welcome",
+  run: (client, message, args) => {
+    const channel = message.mentions.channels.first();
+    const [key, ...value] = args;
+    switch (key) {
+      default:
+        return message.channel.send(
+          new Discord.MessageEmbed()
+            .setColor("RED")
+            .setTimestamp()
+            .setFooter(
+              message.author.tag,
+              message.author.displayAvatarURL({ dynamic: true }) ||
+                client.user.displayAvatarURL({ dynamic: true })
+            )
+            .setDescription("Error: Invalid Key provided, Please try again.")
+        );
+      case "leave": {
+        if (!isURL()) {
+          return message.channel.send(
+            `${client.emotes.error}Pls Give Invalid channel... Try again...`
+          );
+        }
+        db.set(`levchannel_${message.guild.id}`, channel.id);
+        const leave = new Discord.MessageEmbed()
+          .setDescription(
+            `**Done** From now on I will send welcome message in ${channel} when someone joins the server`
+          )
+          .setColor("RED");
+        message.channel.send(leave);
+      }
+        break;
+      case "welcome": {
+        if (!channel) {
+          return message.channel.send(
+            `${client.emotes.error}Pls Give Invalid channel... Try again...`
+          );
+        }
+        db.set(`welchannel_${message.guild.id}`, channel.id);
+        const welcome = new Discord.MessageEmbed()
+          .setDescription(
+            `**Done** From now on I will send welcome message in ${channel} when someone joins the server`
+          )
+          .setColor("RED");
+        message.channel.send(welcome);
+      }
+    }
+  }
+};
