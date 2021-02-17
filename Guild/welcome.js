6c06bf03f452d84dae80f4c1b24c439c10f7064c
Glitch (mammoth-crystalline-vase) <none>
2021-02-17 09:06:38 +0000

    🤘🏃 Checkpoint ./Guild/welcome.js:26780611/41

--- Guild/welcome.js
+++ Guild/welcome.js
@@ -90,9 +90,9 @@ module.exports = function(client) {
       .replace(`{member}`, member) // Member mention substitution
       .replace(`{username}`, member.user.username) // Username substitution
       .replace(`{tag}`, member.user.tag) // Tag substitution
-      .replace(
-        `{date}`, moment(member.joinedAt).format('LLLL'))
-      .replace(`{server}`, member.guild.name) // Name Server substitution
+    /*  .replace(
+        `{date}`, member.guild.joinedAt)
+     */ .replace(`{server}`, member.guild.name) // Name Server substitution
       .replace(`{size}`, member.guild.members.cache.size);
     //  const json = JSON.parse(ch);
     const welcomeembed = new Discord.MessageEmbed()
