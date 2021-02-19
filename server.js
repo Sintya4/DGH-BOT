b761a0797745bfac9517cf3636e93de7ada6c68c
Glitch (mammoth-crystalline-vase) <none>
2021-02-19 01:48:20 +0000

    🐩🐦 Checkpoint ./server.js:26780611/1329

--- server.js
+++ server.js
@@ -37,6 +37,9 @@ client.on("ready", async () => {
    `,
     { type: "WATCHING" }
   );
+var dt = new Date();
+
+console.log("YYYY-MM-DDTHH:MM:SS.MSSZ");
 });
 /*====================================================================*/
 const { readdirSync } = require("fs");
