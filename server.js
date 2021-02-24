426cdb134a61bbebe08e304c2f669fcc70c657de
Glitch (mammoth-crystalline-vase) <none>
2021-02-24 11:43:46 +0000

    🛳🚒 Checkpoint ./server.js:26780611/926

--- server.js
+++ server.js
@@ -32,61 +32,9 @@ Leave(client)
 //<ACTIVITY>
 client.on("ready", async () => {
   console.log(`Bot Is Ready To Go!\nTag: ${client.user.tag}`);
-  client.user.setActivity(
-    `Commands: ${Default_Prefix}help\n ${client.guilds.cache.size} Server | ${client.users.cache.size} User
-   `,
+  client.user.setActivity(`Commands: ${Default_Prefix}help\n ${client.guilds.cache.size} Server | ${client.users.cache.size} User`,
     { type: "WATCHING" }
   );
-});
-
-client.on('ready', () => {
-
-    client.api.applications(client.user.id).guilds('790938885365563392').commands.post({
-
-        data: {
-
-            name: "hello",
-
-            description: "hello world command"
-
-            // possible options here e.g. options: [{...}]
-
-        }
-
-    });
-
-    client.ws.on('INTERACTION_CREATE', async interaction => {
-
-        const command = interaction.data.name.toLowerCase();
-
-        const args = interaction.data.options;
-
-        if (command === 'hello'){ 
-
-            // here you could do anything. in this sample
-
-            // i reply with an api interaction
-
-            client.api.interactions(interaction.id, interaction.token).callback.post({
-
-                data: {
-
-                    type: 4,
-
-                    data: {
-
-                        content: "hello world!!!"
-
-                    }
-
-                }
-
-            })
-
-        }
-
-    });
-
 });
 /*====================================================================*/
 const { readdirSync } = require("fs");
