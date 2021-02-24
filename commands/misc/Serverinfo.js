51bc03ccb2f19d0c0708fcf21cb7cab914f50ecb
Glitch (mammoth-crystalline-vase) <none>
2021-02-24 02:28:17 +0000

    🍍💷 Checkpoint ./commands/misc/Serverinfo.js:26780611/28

--- commands/misc/Serverinfo.js
+++ commands/misc/Serverinfo.js
@@ -5,13 +5,12 @@ module.exports = {
    category: "misc",
   usage: "serverinfo",
   aliases: ['info','stats','serverstats','guildinfo','guildstats'],
-  args: true,
       run: async (client, message, args) => {
    const embed = new Discord.MessageEmbed()  
   .setAuthor(message.guild.name, message.guild.iconURL)
   .setColor(3447003)
   .setDescription(`Owner: ${message.guild.owner.user.tag} (${message.guild.owner.id})`)
-  .addField('Member Count', `${message.guild.memberCount - message.guild.members.filter(m=>m.user.bot).size} (${message.guild.members.filter(m=>m.user.bot).size} bots)`, true)
+  .addField('Member Count', `${message.guild.memberCount - message.guild.members(m=>m.user.bot).size} (${message.guild.members(m=>m.user.bot).size} bots)`, true)
   .addField('AFK Timeout', `${message.guild.afkTimeout / 60} minutes`, true)
   .addField('AFK Channel', `${message.guild.afkChannelID === null ? 'No AFK Channel' : client.channels.get(message.guild.afkChannelID).name} (${message.guild.afkChannelID === null ? '' : message.guild.afkChannelID})`, true)
   .addField('Location', message.guild.region, true)
