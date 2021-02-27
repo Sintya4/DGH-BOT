c8a4dd92e192a3f7e3a1b454f64fe5f087931f4b
Glitch (mammoth-crystalline-vase) <none>
2021-02-27 16:06:01 +0000

    💐🚂 Checkpoint ./commands/admin/code.js:26780611/6515

--- commands/admin/code.js
+++ commands/admin/code.js
@@ -11,16 +11,10 @@ module.exports = {
   run: (client, message, args, mass) => {
     message.delete();
     let code;
-    
- 
+
     try {
- // code = fs.readFileSync(`commands/./${args[0]}.js`).toString();
-       const { readdirSync } = require("fs");
-readdirSync("commands/").forEach(dir => {
-  const code = fs.readFileSync(`commands/${dir}/${args[0]}`).toString();/*.filter(file =>
-    file.endsWith(".js")*/
-  });
-} catch (error) {
+      code = fs.readFileSync(`commands/../${args[0]}.js`).toString();
+    } catch (error) {
       return message.channel.send(
         `I couldn't find a command called \`${args[0]}\``
       );
@@ -38,13 +32,38 @@ readdirSync("commands/").forEach(dir => {
           }
         };
 
-        message.channel.send(
-          `Here is the code for the \`${args[0]}\` command:
+        message.channel
+          .send(
+            `Here is the code for the \`${args[0]}\` command:
 
-\`\`\`js
+\`\`\`kt
 
 ${code.substr(0, 992883)}\`\`\``
-        );
+          )
+          .then(m => {
+            m.react("✅");
+            m.react("❌");
+            const filter = (reaction, user) => {
+              return (
+                ["❌", "✅"].includes(reaction.emoji.name) &&
+                user.id === message.author.id
+              );
+            };
+
+            m.awaitReactions(filter, {
+              max: 1,
+              time: 300000,
+              errors: ["time"]
+            }).then(collected => {
+              const reaction = collected.array()[collected.size - 1];
+
+              if (!reaction.message.guild) return; // If the user was reacting something but not in the guild/server, ignore them.
+
+              if (reaction.emoji.name === "❌") {
+                m.delete();
+              }
+            });
+          });
       }
     } catch (e) {
       return message.channel.send(
