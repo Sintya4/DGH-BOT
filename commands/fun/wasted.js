3e5de3b7a9a64ec0aeee74c093e5f5843e9da3b6
Glitch (mammoth-crystalline-vase) <none>
2021-03-01 16:38:54 +0000

    🏙👨‍👨‍👦 Checkpoint ./commands/fun/wasted.js:26780611/413

---
+++ commands/fun/wasted.js
@@ -0,0 +1,14 @@
+const Random = require ("srod-v12");
+const Discord = require (
+module.exports = {
+        name: "wasted",
+        usage: `wasted <@user>`,
+        category: "category",
+        aliases: ["wtd"],
+        description: "Return A wasted image",
+        args: true,
+        cooldown: 1,
+        permission: "",
+    run: async (client, message, args) => {
+//code
+}}
