322a7216dd82736a2026844af2795b745968ad38
Glitch (mammoth-crystalline-vase) <none>
2021-03-01 16:50:26 +0000

    🛢⛱ Checkpoint ./commands/fun/wasted.js:26780611/415 ./package.json:26780611/3663

--- commands/fun/wasted.js
+++ commands/fun/wasted.js
@@ -1,9 +1,9 @@
 const Random = require ("srod-v12");
-const Discord = require (
+const Discord = require ("discord.js");
 module.exports = {
         name: "wasted",
         usage: `wasted <@user>`,
-        category: "category",
+        category: "fun",
         aliases: ["wtd"],
         description: "Return A wasted image",
         args: true,
@@ -11,4 +11,7 @@ module.exports = {
         permission: "",
     run: async (client, message, args) => {
 //code
-}}
+    const member = message.mentions.first() || message.guild.members.cache.get(args[0]) || message.member
+    const Data = await Random.Wasted({Image: member.user.displayAvatarURL({format : "png" }), Color: "RANDOM"})
+    message.channel.send(Data)
+          }}
