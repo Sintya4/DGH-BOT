507d1c5d2f10f683926392de8817520d1593538a
Glitch (mammoth-crystalline-vase) <none>
2021-03-02 06:51:59 +0000

    👨‍👨‍👦🏉 Checkpoint ./package.json:26780611/3720 ./commands/fun/meme.js:26780611/2319 ./commands/fun/slap.js:26780611/2361 ./commands/fun/sad.js:26780611/1582 ./commands/search/npm.js:26780611/2338

--- commands/fun/meme.js
+++ commands/fun/meme.js
@@ -1,5 +1,9 @@
-const randomPuppy = require('random-puppy');
+const https = require('https');
 const Discord = require("discord.js");
+const url = [
+    'https://www.reddit.com/r/dankmemes/hot/.json?limit=100',
+    'https://www.reddit.com/r/memes/hot/.json?limit=100',
+];
 module.exports = {
         name: "meme",
         usage: `meme`,
@@ -10,41 +14,29 @@ module.exports = {
         permission: "",
     run: async (client, message, args) => {
 //code
-  
-    let reddit = [
-        "meme",
-        "animemes",
-        "MemesOfAnime",
-        "animememes",
-        "AnimeFunny",
-        "dankmemes",
-        "dankmeme",
-        "wholesomememes",
-        "MemeEconomy",
-        "techsupportanimals",
-        "meirl",
-        "me_irl",
-        "2meirl4meirl",
-        "AdviceAnimals"
-    ]
-
-    let subreddit = reddit[Math.floor(Math.random() * reddit.length)];
-
-    message.channel.startTyping();
-
-    randomPuppy(subreddit).then(async url => {
-            await message.channel.send({
-                files: [{
-                    attachment: url,
-                    name: 'meme.png'
-                }]
-            }).then(() => message.channel.stopTyping());
-    }).catch(function (err)  {
-      message.channel.send({embed: {
-         color: 16734039,
-         description: "Something went wrong... :cry:"
-         }})
-      return;
-    })    
-      
-}}
+     https.get(url[Math.floor(Math.random() * url.length)], (result) => {
+            let body = '';
+            result.on('data', (chunk) => {
+                body += chunk;
+            });
+            result.on('end', () => {
+                let response = JSON.parse(body);
+                let index = response.data.children[Math.floor(Math.random() * 99) + 1].data;
+                if (index.post_hint !== 'image') {
+                    return message.channel.send(new Discord.MessageEmbed()
+                        .setAuthor('r/dankmemes')
+                        .setColor('RANDOM')
+                        .setDescription(`[${title}](${client.unicron.serverInviteURL})`));
+                }
+                let image = index.url;
+                let title = index.title;
+                return message.channel.send(new Discord.MessageEmbed()
+                    .setAuthor('r/dankmemes')
+                    .setImage(image)
+                    .setColor('RANDOM')
+                    .setDescription(`[${title}](${client.unicron.serverInviteURL})`));
+            }).on('error', error => {
+                client.logger.error(error);
+                return false;
+            });
+    })}}
