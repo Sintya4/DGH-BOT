8ee4f8a1a112ee46fadc288c5c5a39909bac03a1
Glitch (mammoth-crystalline-vase) <none>
2021-03-02 08:24:30 +0000

    🎀🔔 Checkpoint ./commands/admin/addroleall.js:26780611/6201 ./commands/admin/removeroleall.js:26780611/9152

--- commands/admin/addroleall.js
+++ commands/admin/addroleall.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: "addroleall",
-  aliases: ["arall","aroleall","giveroleall"],
+  aliases: ["arall", "aroleall", "giveroleall"],
   description: "Add a role to all user of the current server",
   category: "admin",
   args: true,
@@ -8,46 +8,95 @@ module.exports = {
   permissions: "MANAGE_ROLES" || "ADMINISTRATOR",
   run: (client, message, args) => {
     if (!message.guild.me.hasPermission("MANAGE_ROLES"))
-      return message.channel.send("I don't have enough permission to do that !");
-   const [key, ...value] = args;
+      return message.channel.send(
+        "I don't have enough permission to do that !"
+      );
+    const [key, ...value] = args;
+  
     switch (key) {
-      case "inbot":
-        {
-              const role =
-      message.guild.roles.cache.find(
-        (role) => role.name === args.join(" ").slice(1)
-      ) || message.mentions.roles.first() || message.guild.roles.cache.get(args.join(" ").slice(1));
+      case "bot": {
+        const role =
+          message.guild.roles.cache.find(
+            role => role.name === args.join(" ").slice(2)
+          ) ||
+          message.mentions.roles.first() ||
+          message.guild.roles.cache.get(args.join(" ").slice(2));
 
-    if (message.guild.me.roles.highest.comparePositionTo(role) < 0) {
-      return message.channel.send(`My role is not high enough than **${role.name}** role!`);
+        if (message.guild.me.roles.highest.comparePositionTo(role) < 0) {
+          return message.channel.send(
+            `My role is not high enough than **${role.name}** role!`
+          );
+        }
+
+        if (message.member.roles.highest.comparePositionTo(role) < 0) {
+          return message.channel.send(
+            `Your role must be higher than **${role.name}** role!`
+          );
+        }
+
+        if (!role) {
+          return message.channel.send("Please provide a valid role");
+        }
+
+        message.guild.bots.cache.forEach(member => member.roles.add(role));
+
+        message.channel.send(
+          `${client.emotes.success} Successfully Added **${role.name}** to Bot`
+        );
+      }
+        break;
+      case "member": {
+        const role =
+          message.guild.roles.cache.find(
+            role => role.name === args.join(" ").slice(2)
+          ) ||
+          message.mentions.roles.first() ||
+          message.guild.roles.cache.get(args.join(" ").slice(2));
+
+        if (message.guild.me.roles.highest.comparePositionTo(role) < 0) {
+          return message.channel.send(
+            `My role is not high enough than **${role.name}** role!`
+          );
+        }
+
+        if (message.member.roles.highest.comparePositionTo(role) < 0) {
+          return message.channel.send(
+            `Your role must be higher than **${role.name}** role!`
+          );
+        }
+
+        if (!role) {
+          return message.channel.send("Please provide a valid role");
+        }
+
+        message.guild.members.cache.forEach(member => member.roles.add(role));
+
+        message.channel.send(
+          `${client.emotes.success} Successfully Added **${role.name}** to Member`
+        );
+      }
+        
+        
     }
+    
 
-    if (message.member.roles.highest.comparePositionTo(role) < 0) {
-      return message.channel.send(`Your role must be higher than **${role.name}** role!`);
-    }
-
-    if (!role) {
-      return message.channel.send("Please provide a valid role");
-    }
-
-    message.guild.members.cache.forEach(member => member.roles.add(role));
-
-    message.channel.send(`${client.emotes.success} Successfully Added **${role.name}** to Everyone`);
-  }
-};
- }}
-break;
     const role =
       message.guild.roles.cache.find(
-        (role) => role.name === args.join(" ").slice(1)
-      ) || message.mentions.roles.first() || message.guild.roles.cache.get(args.join(" ").slice(1));
+        role => role.name === args.join(" ").slice(1)
+      ) ||
+      message.mentions.roles.first() ||
+      message.guild.roles.cache.get(args.join(" ").slice(1));
 
     if (message.guild.me.roles.highest.comparePositionTo(role) < 0) {
-      return message.channel.send(`My role is not high enough than **${role.name}** role!`);
+      return message.channel.send(
+        `My role is not high enough than **${role.name}** role!`
+      );
     }
 
     if (message.member.roles.highest.comparePositionTo(role) < 0) {
-      return message.channel.send(`Your role must be higher than **${role.name}** role!`);
+      return message.channel.send(
+        `Your role must be higher than **${role.name}** role!`
+      );
     }
 
     if (!role) {
@@ -56,6 +105,8 @@ break;
 
     message.guild.members.cache.forEach(member => member.roles.add(role));
 
-    message.channel.send(`${client.emotes.success} Successfully Added **${role.name}** to Everyone`);
-  },
+    message.channel.send(
+      `${client.emotes.success} Successfully Added **${role.name}** to Everyone`
+    );
+  }
 };
