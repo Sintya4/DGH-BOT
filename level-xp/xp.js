d9c013d6c34a4176b21057786e0fea03eeba3855
Glitch (mammoth-crystalline-vase) <none>
2021-03-03 13:31:53 +0000

    👨‍❤️‍💋‍👨🦄 Checkpoint ./level-xp/xp.js:26780611/16

--- level-xp/xp.js
+++ level-xp/xp.js
@@ -36,7 +36,7 @@ class Util {
     if(newlvl > oldlvl) 
    message.channel.send(`${message.author}, You just reached level ${newlvl}`)
     db.add(`xp_${message.author.id}_${message.guild.id}`, toadd)
- if(!oldlvl){
+ if(newlvl) {
       message.channel.send(`${message.author}, You just reached level ${newlvl}`)
    }
   }
