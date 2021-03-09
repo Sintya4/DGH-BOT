c52bde8e7d2b98a062f4bcb8932b0f2a279def79
Glitch (grandiose-crocus-geography) <none>
2021-03-09 11:31:23 +0000

    💾🏘 Checkpoint ./package.json:26780611/3974 ./commands/utility/translate.js:26780611/4122

--- commands/utility/translate.js
+++ commands/utility/translate.js
@@ -1,125 +1,18 @@
 const Discord = require("discord.js");
 const snekfetch = require("snekfetch")
+const translate = require('google-translate-api');
 
 module.exports = {
-  name: "translate",
+  name: "translat",
   aliases: ["translate-"],
-  category: "utility",
+  category: "utilit",
   description: "translate all lang",
-  usage: "translate <text> <lang>",
+  usage: "translate <lang> <text>",
   run: async (client, message, args) => {
-var codes = {
-        "az": "Azerbaijan",
-        "sq": "Albanian",
-        "am": "Amharic",
-        "en": "English",
-        "ar": "Arabic",
-        "hy": "Armenian",
-        "af": "Afrikaans",
-        "eu": "Basque",
-        "ba": "Bashkir",
-        "be": "Belarusian",
-        "bn": "Bengali",
-        "my": "Burmese",
-        "bg": "Bulgarian",
-        "bs": "Bosnian",
-        "cy": "Welsh",
-        "hu": "Hungarian",
-        "vi": "Vietnamese",
-        "ht": "Haitian (Creole)",
-        "gl": "Galician",
-        "nl": "Dutch",
-        "mrj": "Hill Mari",
-        "el": "Greek",
-        "ka": "Georgian",
-        "gu": "Gujarati",
-        "da": "Danish",
-        "he": "Hebrew",
-        "yi": "Yiddish",
-        "id": "Indonesian",
-        "ga": "Irish",
-        "it": "Italian",
-        "is": "Icelandic",
-        "es": "Spanish",
-        "kk": "Kazakh",
-        "kn": "Kannada",
-        "ca": "Catalan",
-        "ky": "Kyrgyz",
-        "zh": "Chinese",
-        "ko": "Korean",
-        "xh": "Xhosa",
-        "km": "Khmer",
-        "lo": "Laotian",
-        "la": "Latin",
-        "lv": "Latvian",
-        "lt": "Lithuanian",
-        "lb": "Luxembourgish",
-        "mg": "Malagasy",
-        "ms": "Malay",
-        "ml": "Malayalam",
-        "mt": "Maltese",
-        "mk": "Macedonian",
-        "mi": "Maori",
-        "mr": "Marathi",
-        "mhr": "Mari",
-        "mn": "Mongolian",
-        "de": "German",
-        "ne": "Nepali",
-        "no": "Norwegian",
-        "pa": "Punjabi",
-        "pap": "Papiamento",
-        "fa": "Persian",
-        "pl": "Polish",
-        "pt": "Portuguese",
-        "ro": "Romanian",
-        "ru": "Russian",
-        "ceb": "Cebuano",
-        "sr": "Serbian",
-        "si": "Sinhala",
-        "sk": "Slovakian",
-        "sl": "Slovenian",
-        "sw": "Swahili",
-        "su": "Sundanese",
-        "tg": "Tajik",
-        "th": "Thai",
-        "tl": "Tagalog",
-        "ta": "Tamil",
-        "tt": "Tatar",
-        "te": "Telugu",
-        "tr": "Turkish",
-        "udm": "Udmurt",
-        "uz": "Uzbek",
-        "uk": "Ukranian",
-        "ur": "Urdu",
-        "fi": "Finnish",
-        "fr": "French",
-        "hi": "Hindi",
-        "hr": "Croatian",
-        "cs": "Czech",
-        "sv": "Swedish",
-        "gd": "Scottish",
-        "et": "Estonian",
-        "eo": "Esperanto",
-        "jv": "Javanese",
-        "ja": "Japanese"
-    }
-    const text = args[0]
-    const to = args[1]
-    const from = args[2]
-        const { body } = await snekfetch
-            .get('https://translate.yandex.net/api/v1.5/tr.json/translate')
-            .query({
-                key: "KEY",
-                text,
-                lang: from ? `${from}-${to}` : to
-            });
-        const lang = body.lang.split('-');
+ translate(args.slice(1).join(" "), {to: args[0]}).then(res => {
         const embed = new Discord.MessageEmbed()
             .setColor(0x00AE86)
-            .addField(`From: ${codes[lang[0]]}`,
-                text)
-            .addField(`To: ${codes[lang[1]]}`,
-                body.text[0]);
-        message.channel.send({embed}).catch(console.error);
- }
-   }
+            .addField("Text",args.slice(1).join(" "))
+            .addField(`To: ${args[0]}`,
+                res.text[0]);
+        message.channel.send(embed)})}}
