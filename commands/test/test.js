2a890dc6e924dedb5dea9bf1a69c6cfae12a697d
Glitch (grandiose-crocus-geography) <none>
2021-03-12 10:21:16 +0000

    🌻💡 Checkpoint ./commands/test/test.js:26780611/2387

--- commands/test/test.js
+++ commands/test/test.js
@@ -10,67 +10,20 @@ module.exports = {
   cooldown: 0,
   permission: "",
   run: async (client, message, args) => {
-    //code
-    var time = '';
-            var time2 = '';
-            var time3 = '';
-            if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('You don\'t have enough permission to execute this command.');
-                const stated_duration_hours = message.content.split(' ')[1];
-                const stated_duration_hours2 = stated_duration_hours.toLowerCase();
-                if (stated_duration_hours2.includes('s')) {
-                    var time = 's';
-                }
-                if (stated_duration_hours2.includes('m')) {
-                    var time = 'm';
-                }
-                if (stated_duration_hours2.includes('h')) {
-                    var time = 'h';
-                }
-                if (stated_duration_hours2.includes('d')) {
-                    var time = 'd';
-                }
-                const stated_duration_hours3 = stated_duration_hours2.replace(time, '');
-                if (stated_duration_hours3 === '0') {
-                    message.channel.send('The duration has to be atleast one.');
-                }
-                if (isNaN(stated_duration_hours3)) {
-                    message.channel.send('The duration has to be a valid time variable.');
-                }
-               if (stated_duration_hours3 > 1) {
-                    var time3 = 's';
-                }
-          if (time === 's') {
-                    var actual_duration_hours = stated_duration_hours3 * 1000;
-                    var time2 = 'second';
-                }
-                if (time === 'm') {
-                    var actual_duration_hours = stated_duration_hours3 * 60000;
-                    var time2 = 'minute';
-                }
-                if (time === 'h') {
-                    var actual_duration_hours = stated_duration_hours3 * 3600000;
-                    var time2 = 'hour';
-                }
-                if (time === 'd') {
-                    var actual_duration_hours = stated_duration_hours3 * 86400000;
-                    var time2 = 'day';
-                }
-  /*   const input = args[0];
-  
-    var result = input.replace(`s`,`second`).replace(`m`,`minute`).replace(`h`,`hour`).replace(`d`,`day`)
-  */  var remainingTime = args[0],
+     const input = args[0].replace(`s`,`second`).replace(`m`,`minute`).replace(`h`,`hour`).replace(`d`,`day`)
+    var remainingTime = args[0],
       remainingCount = 1,
       status = "⏱️";
     var countdown = await message.channel.send(
       new Discord.MessageEmbed()
-        .addField("TIME", `Started! **${stated_duration_hours3}** ${time2}${time3} ${status}`)
+        .addField("TIME", `Started! **${remainingTime}** ${status}`)
         .setColor("RANDOM")
     );
                  let clock = setInterval(() => {
    remainingTime--;
       countdown.edit(
         new Discord.MessageEmbed()
-          .addField("Time", `**${remainingTime}** ${time2}${time3} remain ${status}`)
+          .addField("Time", `**${remainingTime}** remain ${status}`)
           .setColor("RANDOM")
       );
       if (remainingTime == 0) {
@@ -79,10 +32,10 @@ module.exports = {
         countdown.delete();
         message.channel.send(
           new Discord.MessageEmbed()
-            .addField("TIMEAUT", `Done ${time} **${stated_duration_hours3}** ${time2}${time3} ${status}`)
+            .addField("TIMEAUT", `Done **${remainingTime}** $ ${status}`)
             .setColor("RANDOM")
         );
       }
-    }, actual_duration_hours);
+    }, ms(input || 1500));
   }
 }
