db7963497e36b049356f43c39ef617a49476f1bc
Glitch (grandiose-crocus-geography) <none>
2021-03-12 11:04:56 +0000

    ⏳🐦 Checkpoint ./commands/test/test.js:26780611/15603

--- commands/test/test.js
+++ commands/test/test.js
@@ -1,5 +1,6 @@
 const ms = require("ms");
 const Discord = require("discord.js");
+const db = require("quick.db");
 
 module.exports = {
   name: "stopwatch",
@@ -7,40 +8,39 @@ module.exports = {
   category: "category",
   description: "",
   args: true,
-  cooldown: 1,
   permission: "",
+  cooldown: 1,
+
   run: async (client, message, args) => {
     const input = args[0];
     const input2 = args[1];
-    const reply = 1500 || input2.replace(`s`, 1500).replace(`m`, 50000).replace(`h`, 3500000).replace(`d`,86300000)
+    const reply =
+      1500 ||
+      input2
+        .replace(`s` || `second`, 1500)
+        .replace(`m` || `minute`, 50000)
+        .replace(`h` || `hour`, 3500000)
+        .replace(`d` || `day`, 86300000);
     const Ss = reply;
- // Code Cooldown Mode
-const db = require("quick.db")
-const now = Date.now()
-if(db.has(`cd_${message.author.id}`)) {
-  const expirationTime = db.get(`cd_${message.author.id}`) + 3000
-  if(now < expirationTime) {
-  const timeLeft = (expirationTime - now) / 1000;
-		return message.reply(`please wait ${timeLeft.toFixed(1)} more ${input2});
-  }
-}
-  db.set(`cd_${message.author.id}`, now);
-  setTimeout(() => {
-    db.delete(`cd_${message.author.id}`)
-  },3000) 
-  var remainingTime = input,
+    var remainingTime = input,
       remainingCount = 1,
       status = "⏱️";
     var countdown = await message.channel.send(
       new Discord.MessageEmbed()
-        .addField("Loading-Time", `Started! **${remainingTime}${input2 || "s"}** ${status}`)
+        .addField(
+          "Loading-Time",
+          `Started! **${remainingTime}${input2 || "s"}** ${status}`
+        )
         .setColor("RANDOM")
     );
     let clock = setInterval(() => {
       remainingTime--;
       countdown.edit(
         new Discord.MessageEmbed()
-          .addField("Start-Time", `**${remainingTime}${input2 || "s"}** remain ${status}`)
+          .addField(
+            "Start-Time",
+            `**${remainingTime}${input2 || "s"}** remain ${status}`
+          )
           .setColor("RANDOM")
       );
       if (remainingTime == 0) {
@@ -49,7 +49,10 @@ if(db.has(`cd_${message.author.id}`)) {
         countdown.delete();
         message.channel.send(
           new Discord.MessageEmbed()
-            .addField("Stop-Time", `Done **${remainingTime}${input2 || "s"}** ${status}`)
+            .addField(
+              "Stop-Time",
+              `Done **${remainingTime}${input2 || "s"}** ${status}`
+            )
             .setColor("RANDOM")
         );
       }
