55a7deddc50122b9afc15fb84c5151c2b7b678fb
Glitch (grandiose-crocus-geography) <none>
2021-03-12 16:08:05 +0000

    💨🐒 Checkpoint ./commands/misc/level.js:26780611/1828 ./package.json:26780611/4113

--- commands/misc/level.js
+++ commands/misc/level.js
@@ -1,7 +1,54 @@
 const db = require("quick.db");
 const discord = require("discord.js");
 const { getInfo } = require("../../level-xp/xp.js");
+const canvacord = require("canvacord");
+const Discord = require("discord.js");
 module.exports = {
+  name: "level",
+  aliases: ["lvl", "rank"],
+  description: "Get the level of Author or Mentioned",
+  usage: "level [user]",
+  category: "info",
+  run: (client, message, args) => {
+    const user = message.mentions.users.first() || message.author;
+
+    if(user.id === client.user.id) { //IF BOT
+      return message.channel.send(":wink: | I am on level 100")
+    }
+
+    if(user.bot) {
+      return message.channel.send("Bot do not have levels")
+    }
+
+   let xp = db.get(`xp_${user.id}_${message.guild.id}`) || 0;
+    const {level, remxp, levelxp} = getInfo(xp);
+
+const rank = new canvacord.Rank()
+    .setAvatar(user.displayAvatarURL({dynamic: false,  format: 'png'}))
+    .setCurrentXP(remxp)
+    .setRequiredXP(levelxp)
+    .setLevel(level)
+    .setStatus(user.presence.status)
+    .setProgressBar("#00FFFF", "COLOR")
+    .setUsername(user.username)
+    .setDiscriminator(user.discriminator)
+    .setRank(1, "a", false)
+    .setBackground("IMAGE", "https://cdn.discordapp.com/attachments/815383261966499840/815384721227382784/card.jpg%22");
+
+rank.build()
+    .then(data => {
+        const attachment = new Discord.MessageAttachment(data, "automodRankcard.png");
+        message.channel.send(attachment);
+    });
+
+
+
+
+  }
+}
+
+
+/*module.exports = {
   name: "level",
   description: "Get the level of author or mentioned",
   usage: "level <user>",
@@ -24,7 +71,6 @@ module.exports = {
     const { level, remxp, levelxp } = getInfo(xp);
     if (xp === 0)
       return message.channel.send(`**${user.tag}** is out of the xp`);
-
     let embed = new discord.MessageEmbed()
       .setAuthor(user.username, message.guild.iconURL())
       .setColor("#ff2050")
@@ -36,3 +82,4 @@ module.exports = {
     message.channel.send(embed);
   }
 };
+*/
