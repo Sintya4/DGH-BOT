c74abeb61190795a783a8d5aa859d624bcfe4475
Glitch (grandiose-crocus-geography) <none>
2021-03-12 10:35:30 +0000

    📊🍜 Checkpoint ./commands/test/test.js:26780611/2883

--- commands/test/test.js
+++ commands/test/test.js
@@ -10,8 +10,11 @@ module.exports = {
   cooldown: 0,
   permission: "",
   run: async (client, message, args) => {
-     const input = args[0].replace(`s`,`second`).replace(`m`,`minute`).replace(`h`,`hour`).replace(`d`,`day`)
-    var remainingTime = args[0],
+    const input = args[0];
+    const input2 = args[1];
+    const reply = input2.replace(`s`, 1500).replace(`m`, 50000).replace(`h`, 3500000).replace(`d`,86300000)
+    const Ss = reply;
+    var remainingTime = input,
       remainingCount = 1,
       status = "⏱️";
     var countdown = await message.channel.send(
@@ -19,8 +22,8 @@ module.exports = {
         .addField("TIME", `Started! **${remainingTime}** ${status}`)
         .setColor("RANDOM")
     );
-                 let clock = setInterval(() => {
-   remainingTime--;
+    let clock = setInterval(() => {
+      remainingTime--;
       countdown.edit(
         new Discord.MessageEmbed()
           .addField("Time", `**${remainingTime}** remain ${status}`)
@@ -32,10 +35,10 @@ module.exports = {
         countdown.delete();
         message.channel.send(
           new Discord.MessageEmbed()
-            .addField("TIMEAUT", `Done **${remainingTime}** $ ${status}`)
+            .addField("TIMEAUT", `Done **${remainingTime}** ${status}`)
             .setColor("RANDOM")
         );
       }
-    }, ms(input || 1500));
+    }, Ss || 1500);
   }
-}
+};
