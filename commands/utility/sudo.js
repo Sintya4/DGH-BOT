d79c2a90a3cbf754684a35e722f3c6f41b92e4e5
Glitch (grandiose-crocus-geography) <none>
2021-03-13 09:02:04 +0000

    😽🛥 Checkpoint ./commands/utility/sudo.js:26780611/4591

--- commands/utility/sudo.js
+++ commands/utility/sudo.js
@@ -6,7 +6,7 @@ module.exports = {
   args: true,
   cooldown: 5,
   run: async (client, message, args) => {
-     message.delete();
+    message.delete();
     let user =
       message.mentions.members.first() ||
       message.guild.members.cache.get(args[0]);
@@ -15,6 +15,26 @@ module.exports = {
       avatar: user.user.displayAvatarURL(),
       channel: message.channel.id
     });
+    const webhook2 = await message.channel.createWebhook(user.displayName, {
+      avatar: user.user.displayAvatarURL(),
+      channel: message.channel.id
+    });
+    const [key, ...value] = args;
+    switch (key) {
+      case "-json": {
+        try {
+          const json = JSON.parse(args.slice(2).join(" "));
+          await webhook.send({
+            embed: json
+          });
+          webhook.delete();
+        } catch (error) {
+          return message.channel.send(
+            "go to the web: https://embedbuilder.nadekobot.me/" //  `\`\`\`\n$ embed -json {"title": "My title","color":"Name color","description": "My description"}\n\`\`\`\`\`\`\n$ embed -json {"author": {"name": "My author name", "icon_url": "url here"}, "description": "My description"}\n\`\`\`\`\`\`\n$ embed -json {"fields": [{"name": "My field name", "value": "My field value"}, {"name": "My field name", "value": "My field value", "inline": false}]}\n\`\`\``
+          );
+        }
+      }
+    }
     await webhook.send(args.slice(1).join(" ")).then(() => {
       webhook.delete();
     });
