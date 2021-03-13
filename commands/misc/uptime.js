36f5b9b9f8447ce33fe951517543ab890f8469b7
Glitch (grandiose-crocus-geography) <none>
2021-03-13 09:15:44 +0000

    👫🍟 Checkpoint ./commands/utility/sudo.js:26780611/1438 ./commands/misc/uptime.js:26780611/59

--- commands/misc/uptime.js
+++ commands/misc/uptime.js
@@ -13,39 +13,22 @@ module.exports = {
 
   run: async (client, message, args) => {
     message.delete();
-
     let seconds = Math.floor(message.client.uptime / 1000);
-
     let minutes = Math.floor(seconds / 60);
-
     let hours = Math.floor(minutes / 60);
-
     let days = Math.floor(hours / 24);
-
-    //  let ssay = Math.floor(days / 1)
-
     let sunday = Math.floor(days / 7);
-
     seconds %= 60;
-
     minutes %= 60;
-
     hours %= 24;
-
     sunday %= 7;
-
     const u = message.mentions.members.first();
-
     const us = message.guild.members.fetch(client.user.id);
-
     const ddi = `790937024941129759`;
-
     const uptime = new MessageEmbed()
-
       .setTitle(
         "<:slowmode:799034410799595590> TIME BOT ONLINE <:slowmode:799034410799595590>"
       )
-
       .setDescription(
         `<a:success:798526789114134548> <@${ddi}> : 
 \`Second: ${seconds}
@@ -53,11 +36,8 @@ Minutes: ${minutes}
 Hours: ${hours}
 day: ${days}\``
       )
-
       .setColor("GREEN")
-
       .setTimestamp();
-
     return message.channel.send(uptime);
   }
 };
--- commands/utility/sudo.js
+++ commands/utility/sudo.js
@@ -15,26 +15,6 @@ module.exports = {
       avatar: user.user.displayAvatarURL(),
       channel: message.channel.id
     });
-    const webhook2 = await message.channel.createWebhook(user.displayName, {
-      avatar: user.user.displayAvatarURL(),
-      channel: message.channel.id
-    });
-    const [key, ...value] = args;
-    switch (key) {
-      case "-json": {
-        try {
-          const json = JSON.parse(args.slice(2).join(" "));
-          await webhook.send({
-            embed: json
-          });
-          webhook.delete();
-        } catch (error) {
-          return message.channel.send(
-            "go to the web: https://embedbuilder.nadekobot.me/" //  `\`\`\`\n$ embed -json {"title": "My title","color":"Name color","description": "My description"}\n\`\`\`\`\`\`\n$ embed -json {"author": {"name": "My author name", "icon_url": "url here"}, "description": "My description"}\n\`\`\`\`\`\`\n$ embed -json {"fields": [{"name": "My field name", "value": "My field value"}, {"name": "My field name", "value": "My field value", "inline": false}]}\n\`\`\``
-          );
-        }
-      }
-    }
     await webhook.send(args.slice(1).join(" ")).then(() => {
       webhook.delete();
     });
