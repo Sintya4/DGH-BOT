124994945bd248a8d2e0e7d06e3b6186e126f4da
Glitch (grandiose-crocus-geography) <none>
2021-03-13 00:08:52 +0000

    🏋🌘 Checkpoint ./commands/utility/stopwatch.js:26780611/177

--- commands/utility/stopwatch.js
+++ commands/utility/stopwatch.js
@@ -13,14 +13,13 @@ module.exports = {
 
   run: async (client, message, args) => {
     const input = args[0];
-    const input2 = args[1];
+    const input2 = args[1] || "s";
     const reply = input2
       .replace(`s` || `second` || `detik`, 1500)
       .replace(`m` || `minute` || `menit`, 60000)
       .replace(`h` || `hour` || `jam`, 3600000)
       .replace(`d` || `day` || `hari`, 86300000);
     const Ss = reply;
-    if(input2){return message.channel.send("}
     var remainingTime = input,
       remainingCount = 1,
       status = "⏱️";
@@ -55,6 +54,6 @@ module.exports = {
             .setColor("RANDOM")
         );
       }
-    }, reply || 1500);
+    }, reply);
   }
 };
