26c64709128851ab61a5586cb52a1a72c7035901
Glitch (grandiose-crocus-geography) <none>
2021-03-19 05:43:48 +0000

    🏗📷 Checkpoint ./server.js:26780611/5161 ./events/message.js:26780611/4

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const Discord = require("discord.js");
 const fs = require("fs");
-const client = new Discord.Client({
+const { Client } = require("discord.js");
+const db = require("quick.db");
+const ms = require("pretty-ms");
+const mongoose = require("mongoose");
+const { MessageEmbed } = require("discord.js");
+const client = new Client({
   disableEveryone: true
 });
 const {
@@ -12,12 +17,34 @@ const {
   DateDat,
   Dashboard
 } = require("./config.js");
+const { addexp } = require("./level-xp/xp.js");
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 const cooldowns = new Discord.Collection();
 client.queue = new Map();
-client.config = require("../emoji/emojis");
+client.config = require("./emoji/emojis");
 client.emotes = client.config.emojis;
+
+const fetch = require("node-fetch");
+setInterval(async () => {
+  await fetch("https://randiose-crocus-geography.glitch.me/").then(
+    console.log("My Live")
+  );
+}, 240000);
+setInterval(async () => {
+  await fetch("https://bot-jsll.glitch.me/").then(
+    console.log("My Dashboard Ping")
+  );
+}, 240000);
+
+client.on("ready", async () => {
+  console.log(`Bot Is Ready To Go!\nTag: ${client.user.tag}`);
+  client.user.setStatus("dnd");
+  client.user.setActivity(
+    `Commands: ${Default_Prefix}help\n ${client.guilds.cache.size} Server | ${client.users.cache.size} User`,
+    { type: "WATCHING" }
+  );
+});
 
 const { readdirSync } = require("fs");
 readdirSync("./commands/").forEach(dir => {
@@ -49,6 +76,137 @@ for (let file of fs.readdirSync("./events/")) {
   }
 }
 
+client.snipe = new Map();
+client.on("messageDelete", function(message, channel) {
+  client.snipe.set(message.channel.id, {
+    content: message.content,
+    author: message.author.tag,
+    image: message.attachments.first()
+      ? message.attachments.first().proxyURL
+      : null
+  });
+});
+
+//<SETUP>
+client.on("message", async message => {
+  if (message.author.bot || !message.guild || message.webhookID) return;
+  let Prefix = await db.get(`Prefix_${message.guild.id}`);
+  if (!Prefix) Prefix = Default_Prefix;
+  const escapeRegex = str =>
+    str.replace(/[.<>`•√π÷×¶∆£¢€¥*@_+?^${}()|[\]\\]/g, "\\$&");
+  const prefixRegex = new RegExp(
+    `^(<@!?${client.user.id}>|${escapeRegex(Prefix)})\\s*`
+  );
+  if (!prefixRegex.test(message.content)) return;
+  const [, matchedPrefix] = message.content.match(prefixRegex);
+  const args = message.content
+    .slice(matchedPrefix.length)
+    .trim()
+    .split(/ +/);
+  let cmd = args.shift().toLowerCase();
+  let command =
+    client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd));
+  if (!command) return;
+  //<COMMAND USAGE AND DESCRIPTION>
+  if (command.args && !args.length) {
+    return message.channel.send(
+      new MessageEmbed()
+        .setColor("RED")
+        .setTimestamp()
+        .setDescription(
+          `You didn't provide any arguments, ${
+            message.author
+          }!\nThe proper usage would be: \n\`\`\`html\n${command.usage ||
+            "No Usage"}\n\`\`\`Description:\`\`\`html\n${command.description ||
+            "No Description"}\n\`\`\``
+        )
+    );
+  }
+  if (command.guildOnly && message.channel.type === "dm") {
+    return message.reply("I can't execute that command inside DMs!");
+  }
+  if (command.owner && message.author.id != `${message.guild.ownerID}`) {
+    const owmer = new MessageEmbed()
+      .setColor("RED")
+      .setDescription(
+        "<a:failed:798526823976796161>These commands can only be used by owner"
+      );
+
+    return message.channel
+      .send(owmer)
+      .then(m => m.delete({ timeout: 20000 }).catch(e => {}));
+  }
+  if (command.permissions) {
+    const authorPerms = message.channel.permissionsFor(message.author);
+    if (
+      !authorPerms ||
+      !authorPerms.has(command.permissions || "ADMINISTRATOR")
+    ) {
+      return message.channel.send(
+        new MessageEmbed()
+          .setColor("RED")
+          .setTimestamp()
+          .setDescription(
+            `You do not have permission to use this command.\nThis command requires \`${command.permissions ||
+              "ADMINISTRATOR"}\``
+          )
+      );
+    }
+  }
+  if (command.botPermission) {
+    let neededPerms = []
+
+    command.botPermission.forEach(p => {
+      if (!message.guild.me.hasPermission(p)) neededPerms.push("`" + p + "`")
+    })
+
+    if (neededPerms.length) return message.channel.send(`I need ${neededPerms.join(", ")} permission(s) to execute the command!`)
+  }
+  if (!cooldowns.has(command.name)) {
+    cooldowns.set(command.name, new Discord.Collection());
+  }
+  const now = Date.now();
+  const timestamps = cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldown || 3) * 1000;
+  if (timestamps.has(message.author.id)) {
+    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+    if (now < expirationTime) {
+      const timeLeft = (expirationTime - now) / 1000;
+      return message.channel.send(
+        new MessageEmbed()
+          .setColor("RED")
+          .setTimestamp()
+          .setDescription(
+            `<a:failed:798526823976796161> Please wait **${ms(
+              timeLeft
+            )}** before reusing the command again.`
+          )
+      );
+    }
+  }
+  timestamps.set(message.author.id, now);
+  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+  try {
+    if (command) {
+      command.run(client, message, args);
+    }
+  } catch (error) {
+    const errrr = new MessageEmbed()
+      .setColor("RED")
+      .setTimestamp()
+      .setDescription(
+        `Something went wrong executing that command\nError Message: \`${
+          error.message ? error.message : error
+        }\``
+      );
+    return message.channel
+      .send(errrr)
+      .then(m => m.delete({ timeout: 13000 }).catch(e => {}));
+
+    client.logger.error(error);
+  }
+  return addexp(message);
+});
 client
   .login(Token)
   .catch(() =>
