c5b96717ac86dd26ddeebde09e70a5aca1719234
Glitch (grandiose-crocus-geography) <none>
2021-03-19 06:54:20 +0000

    🍋🏈 Checkpoint ./commands/admin/addxp.js:26780611/445

--- commands/admin/addxp.js
+++ commands/admin/addxp.js
@@ -11,7 +11,12 @@ module.exports = {
         owner: true,
     run: async (client, message, args) => {
 //code
-      const toadd = args
-         db.add(`xp_${message.author.id}_${message.guild.id}`, toadd)
- 
+      const toadd = args.slice(1).join(" ")
+      const member = args[0]
+  if(isNaN(member)) 
+    return message.channel.send("pls give id user")
+      
+     
+    await db.add(`xp_${message.author.id}_${message.guild.id}`, toadd)
+ return message.channel.send("managed to add XP level with "+ `<@${member}>`+ "as much" +toadd)
 }}
