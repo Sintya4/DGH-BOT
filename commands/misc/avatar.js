33546157b5f9ecaaaf06b1de223bdc6d6647a5c3
Glitch (grandiose-crocus-geography) <none>
2021-03-20 03:09:49 +0000

    👶💍 Checkpoint ./commands/misc/avatar.js:26780611/4271

--- commands/misc/avatar.js
+++ commands/misc/avatar.js
@@ -1,42 +1,49 @@
 const { MessageEmbed } = require("discord.js");
-const Discord = require('discord.js')
+const Discord = require("discord.js");
 const color = `GREEN`;
-const { Client: DiscordClient, Collection,
+const {
+  Client: DiscordClient,
+  Collection,
 
-    Message, Emoji, Guild,
+  Message,
+  Emoji,
+  Guild,
 
-    GuildEmoji, User, Channel, Role, GuildMember
-
-} = require('discord.js');
+  GuildEmoji,
+  User,
+  Channel,
+  Role,
+  GuildMember
+} = require("discord.js");
 module.exports = {
   name: "avatar",
   category: "misc",
-    aliases: ['av'],
+  aliases: ["av"],
   usage: "avatar <user>",
-  args: true,
+  args: false,
 
-              
-  run: async (client, message, args, ) => {
-      let user 
-    message.delete()
-    let clo 
-  if (message.mentions.users.first()) {
-    user = message.mentions.users.first()
-  clo = client.fetchUser(args[0])
-  
-  let avatar = user.displayAvatarURL({dynamic: true});
-  // 4096 is the new biggest size of the avatar.
-  // Enabling the dynamic, when the user avatar was animated/GIF, it will result as a GIF format.
-  // If it's not animated, it will result as a normal image format.
-  
-  const embed = new Discord.MessageEmbed()
-  .setTitle(`@${user.tag} avatar`)
-  .setDescription(`**[Avatar URL of @${user.tag}](${avatar})**`)
-  .setColor(color)
-  .setImage(avatar)
-  
-  return message.channel.send(embed).then(m=>m.delete({timeout: 40000})
-    )}}
-  }
+  run: async (client, message, args) => {
+    message.delete();
+     const user =
+        message.mentions.users.first() || message.author || message.guild.users.fetch(args[0])
     
-     
+
+    if (!user) {
+      return message.channel.send(
+        "<a:failed:798526823976796161> Unable to find this person!"
+      );
+    }
+    let avatar = user.displayAvatarURL({ dynamic: true });
+    // 4096 is the new biggest size of the avatar.
+    // Enabling the dynamic, when the user avatar was animated/GIF, it will result as a GIF format.
+    // If it's not animated, it will result as a normal image format.
+
+    const embed = new Discord.MessageEmbed()
+      .setTitle(`@${user.tag} avatar`)
+      .setDescription(`**[Avatar URL of @${user.tag}](${avatar})**`)
+      .setColor(color)
+      .setImage(avatar);
+
+    return message.channel.send(embed).then(m => m.delete({ timeout: 40000 }));
+  }
+};
